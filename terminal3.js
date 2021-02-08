8aee6d689d8b8483658c221ef99b6f1ca282aefb
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2021-02-08 14:11:39 +0100

    create new instruction for terminal3

--- terminal3.js
+++ terminal3.js
@@ -1,1 +1,72 @@
-console.log("hello word")
+console.warn("Welcome")
+
+let school = []
+
+let entranceResult = [
+    {
+        id:001,
+        studentName:"helen",
+        examPoint: 85
+    },
+    {
+        id:001,
+        studentName:"adam",
+        examPoint: 60
+    },
+
+
+    {
+        id:002,
+        studentName:"patrick",
+        examPoint: 75
+    },
+    {
+        id:002,
+        studentName:"nelson",
+        examPoint: 60
+    },
+
+
+    {
+        id:003,
+        studentName:"wilson",
+        examPoint: 65
+    },
+    {
+        id:003,
+        studentName:"willian",
+        examPoint: 50
+    },
+
+
+    {
+        id:004,
+        studentName:"olson",
+        examPoint: 50
+    },
+    {
+        id:004,
+        studentName:"jeffery",
+        examPoint: 55
+    },
+
+    {
+        id:005,
+        studentName:"caroline",
+        examPoint: 80
+    },
+
+    {
+        id:006,
+        studentName:"qunicy",
+        examPoint: 55
+    },
+
+    {
+        id:007,
+        studentName:"catherine",
+        examPoint: 70
+    },
+
+
+]
